3415bc2483efeeb27f63b0b8f08aaf59e6ef9833
ereizas <ereizas@gmail.com>
2025-07-12 16:13:34 -0400

    Color grid squares blue on mouse over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,15 @@ for(let i = 0; i<NUM_GRIDS; ++i){
     let gridRow = document.createElement("div");
     gridRow.setAttribute("class","grid-row");
     for(let j = 0; j<NUM_GRIDS; ++j){
-        gridRow.appendChild(document.createElement("div"));
+        grid = document.createElement("div");
+        grid.setAttribute("class","grid-square");
+        gridRow.appendChild(grid);
     }
     gridContainer.appendChild(gridRow);
 }
+
+gridContainer.addEventListener("mouseover", function(event) {
+    if(event.target.classList.contains("grid-square")){
+        event.target.style.backgroundColor = "blue";
+    }
+})
