fd49985e934c13f041038f5bea153b00e7f67e82
ereizas <ereizas@gmail.com>
2025-07-12 16:29:24 -0400

    Prompt user for grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,20 @@
-const NUM_GRIDS = 16;
+let num_grids = 16;
+let btn = document.querySelector("button");
+btn.addEventListener("click", function(){
+    while(1){
+        let temp = Number(prompt("Enter the number of grids you want (up to 100):","16"));
+        if(Number.isInteger(temp) && temp>0 && temp<=100){
+            num_grids = temp;
+            break;
+        }
+    }
+})
+
 let gridContainer = document.querySelector("#grid-container");
-for(let i = 0; i<NUM_GRIDS; ++i){
+for(let i = 0; i<num_grids; ++i){
     let gridRow = document.createElement("div");
     gridRow.setAttribute("class","grid-row");
-    for(let j = 0; j<NUM_GRIDS; ++j){
+    for(let j = 0; j<num_grids; ++j){
         grid = document.createElement("div");
         grid.setAttribute("class","grid-square");
         gridRow.appendChild(grid);
